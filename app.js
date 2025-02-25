e6f873d0a0d7e0209f044d399270d5a26e794430
theresajvrensburg <theresa@isogramconsulting.com>
2025-02-25 19:44:08 +0200

    project completed

--- app.js
+++ app.js
@@ -11,8 +11,15 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const mongoose = require("mongoose");
+ 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mongoose-recipes")
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to mongo", err));
 
 
+const Recipe = require("./models/Recipe.model");
 
 // ROUTES
 //  GET  / route - This is just an example route
@@ -23,23 +30,73 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post('/recipes', (req, res) => {
+    console.log(req.body);
+    // req.body contains the data sent by the client. 
+    // This must match the structure defined in our Book schema.
+    // Recipe.create(req.body)
+    // // OR
+    Recipe.create({
+      title: req.body.title,
+      instructions: req.body.instructions,
+      level: req.body.level,
+      ingredients: req.body.ingredients,
+      image: req.body.image,
+      duration: req.body.duration,
+      isArchived: req.body.isArchived,
+      created: req.body.created
+    })
+      .then((createdRecipe) => {
+          console.log("Recipe created ->", createdRecipe);
+          res.status(201).json(createdRecipe);
+    })
+      .catch((error) => {
+        console.error("Error while creating the recipe ->", error);
+        res.status(500).json({ error: "Failed to create the recipe" });
+      });
+});
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get('/recipes', (req, res) => {
+    Recipe.find().then((allRecipes)=> {
+        res.status(200).json(allRecipes);
+    }).catch((error)=>{
+        res.status(500).json({ message: "Error while retrieving all recipes"})
+    })
+});
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
+app.get('/recipes/:id', (req, res) => {
+    Recipe.findById(req.params.id).then((recipe)=> {
+        res.status(200).json(recipe);
+    }).catch((error)=>{
+        res.status(500).json({ message: "Error while retrieving the recipe"})
+    })
+});
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
+app.put('/recipes/:id', (req, res) => {
+    Recipe.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then((updatedRecipe)=> {
+        res.status(200).json(updatedRecipe);
+    }).catch((error)=>{
+        res.status(500).json({ message: "Error while updating the single recipe"})
+    })
+});
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
-
+app.delete('/recipes/:id', (req, res) => {
+    Recipe.findByIdAndDelete(req.params.id)
+    .then(()=> {
+        res.status(204).send();
+    }).catch((error)=>{
+        res.status(500).json({ message: "Error while attempting to delete a recipe"})
+    })
+});
 
 
 // Start the server
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,29 @@
 // Your code here ...
+// ./models/Recipe.model.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+// CREATE SCHEMA
+// Schema - describes and enforces the structure of the documents
+const recipeSchema = new Schema({
+  title: { type: String, required: true, unique: true },
+  instructions: { type: String, required: true},
+  level: { type: String, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"] },
+  ingredients: {type: [String]},
+  image: { type: String, default: "https://images.media-allrecipes.com/images/75131.jpg"},
+  duration: {type: Number, min: 0},
+  isArchived: {type: Boolean, default: false},
+  created: { type: Date, default: Date.now }
+});
+
+
+// CREATE MODEL
+// The model() method defines a model (Recipe) and creates a collection (recipes) in MongoDB
+// The collection name will default to the lowercased, plural form of the model name:
+//                          "Recipe" --> "recipes"
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+
+// EXPORT THE MODEL
+module.exports = Recipe;
